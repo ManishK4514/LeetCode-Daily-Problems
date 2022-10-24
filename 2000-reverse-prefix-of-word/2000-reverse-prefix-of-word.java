ed3e2b679c73c388a71f7f5b9129c43cb7863b80
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-10-24 18:25:13 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (99.53%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -1,20 +1,20 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
-        int i = 0;
-        StringBuilder sb = new StringBuilder();
-        while(i < word.length()){
-            sb.append(word.charAt(i));
-            if(word.charAt(i) == ch){
+        char[] c = word.toCharArray();
+        int locate = 0;
+        for (int i = 0; i < word.length(); i++) { 
+            if (ch == c[i]) {
+                locate = i;
                 break;
-            }            
-            i++;
+            }
         }
-        if(sb.length() == word.length() && word.charAt(word.length() - 1) != ch){
-            return word;
+        char[] res = new char[word.length()];
+        for (int i = 0; i <= locate; i++) {
+            res[i] = c[locate - i];
         }
-        sb.reverse();
-        String ans = sb.toString();
-        ans += word.substring(i + 1, word.length());
-        return ans;
+        for (int i = locate + 1; i < word.length(); i++) {
+            res[i] = c[i];
+        }
+        return String.valueOf(res);
     }
 }
