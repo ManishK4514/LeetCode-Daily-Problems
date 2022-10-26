6805dc7bd8b2c06187256524abca06034bd4276c
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-10-26 17:28:54 +0530

    Time: 2 ms (65.03%), Space: 42 MB (40.03%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
@@ -1,22 +1,5 @@
 class Solution {
     public String generateTheString(int n) {
-        StringBuilder sb = new StringBuilder();
-        if(n % 2 == 0){
-            int i = 1;
-            while(i < n){
-                sb.append("a");
-                i++;
-            }
-            sb.append("b");
-            return sb.toString();
-        }
-        else{
-            int i = 1;
-            while(i <= n){
-                sb.append("a");
-                i++;
-            }
-            return sb.toString();
-        }
+        return "b" + "ab".substring(n % 2, 1 + n % 2).repeat(n - 1);
     }
 }
