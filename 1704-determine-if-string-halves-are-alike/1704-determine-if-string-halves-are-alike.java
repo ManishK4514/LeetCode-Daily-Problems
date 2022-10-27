14af115419a94b2c2170fb051a24bdbdcdf230ed
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-10-27 21:42:18 +0530

    Time: 9 ms (46.80%), Space: 42.3 MB (60.39%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,32 +1,13 @@
 class Solution {
-    public int countVowels(String str, HashSet<Character> set){
-        int count = 0;
-        for(int i = 0; i < str.length(); i++){
-            if(set.contains(str.charAt(i))){
-                count++;
-            }
-        }
-        return count;
-    }
     public boolean halvesAreAlike(String s) {
-        HashSet<Character> set = new HashSet<>();
-        set.add('a');
-        set.add('e');
-        set.add('i');
-        set.add('o');
-        set.add('u');
-        set.add('A');
-        set.add('E');
-        set.add('I');
-        set.add('O');
-        set.add('U');
-        String a = s.substring(0, s.length()/2);
-        String b = s.substring(s.length()/2, s.length());
-        int left = countVowels(a, set);
-        int right = countVowels(b, set);
-        if(left == right){
-            return true;
+        Set<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+
+        int count = 0, i = 0, j = s.length() - 1;
+
+        while (i < j) {
+            if (set.contains(s.charAt(i++))) ++count;
+            if (set.contains(s.charAt(j--))) --count;
         }
-        return false;
+        return count == 0;
     }
 }
