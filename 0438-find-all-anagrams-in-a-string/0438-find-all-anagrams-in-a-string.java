006b5f5a0141f924d9cc00ad7245d6cd6cab7a13
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-10-28 12:32:29 +0530

    Time: 36 ms (49.48%), Space: 47.6 MB (63.15%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,30 +1,40 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())return false;
-     
-        int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-          freq[s.charAt(i) - 'a']++;
-        }
-        for (int i = 0; i < t.length(); i++) {
-          freq[t.charAt(i) - 'a']--;
-        }
-        for (int i = 0; i < 26; i++) {
-          if (freq[i] != 0)
-            return false;
+    public static boolean check(int[] phash, int[] hash){
+        for(int i = 0; i < phash.length; i++){
+            if(phash[i] != hash[i]){
+                return false;
+            }
         }
         return true;
     }
     public List<Integer> findAnagrams(String s, String p) {
-        int k = p.length();
         List<Integer> ans = new ArrayList<>();
-        int i = 0;
-        for(int j = k; j <= s.length(); j++){
-            boolean possible = isAnagram(s.substring(i, j), p);
-            if(possible){
-                ans.add(i);
+        if(s.length() < p.length()){
+            return ans;            
+        }
+        int[] phash = new int[26];
+        int[] hash = new int[26];
+        int left = 0, right = 0, window = p.length();
+        while(right < window){
+            phash[p.charAt(right) - 'a']++;
+            hash[s.charAt(right) - 'a']++;
+            right++;
+        }
+        right -=1;
+        while(right < s.length())
+        {            
+            boolean equal = check(phash, hash);
+            if(equal)
+            {
+                ans.add(left);
             }
-            i++;
+            right+=1;
+            if(right != s.length())
+            {
+                hash[s.charAt(right) - 'a'] += 1;
+            }
+            hash[s.charAt(left) - 'a'] -=1 ;
+            left += 1;
         }
         return ans;
     }
