a7fbd952aa0f353db394c277656dce3200096c55
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-11-08 11:44:39 +0530

    Time: 8 ms (26.21%), Space: 42.8 MB (57.53%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,26 +1,22 @@
 class Solution {
     public String makeGood(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        while(true){
-            boolean flag = true;
-            for(int i = 0; i < sb.length(); i++){
-                if(sb.charAt(i) >= 'A' && sb.charAt(i) <= 'Z'){
-                    if(i < sb.length() - 1 && (char)(sb.charAt(i) + 32) == sb.charAt(i+1)){
-                        sb.deleteCharAt(i);
-                        sb.deleteCharAt(i);
-                        flag = false;
-                    }
-                    else if(i > 0 && (char)(sb.charAt(i) + 32) == sb.charAt(i-1)){
-                        sb.deleteCharAt(i - 1);
-                        sb.deleteCharAt(i - 1);
-                        flag = false;
-                    }
-                }
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(!st.empty() && (char)(s.charAt(i) + 32) == st.peek()){
+                st.pop();                
             }
-            if(flag){
-                break;
+            else if(!st.empty() && (char)(s.charAt(i) - 32) == st.peek()){
+                st.pop();
+            }
+            else{
+                st.push(s.charAt(i));
             }
         }
-        return sb.toString();
+        char[] ch = new char[st.size()];
+        int i = st.size() - 1;
+        while(!st.empty()){
+            ch[i--] = st.pop();
+        }
+        return new String(ch);
     }
 }
