6c5a521b318847736315d1f9a9ef6ca640ef56e6
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-11-14 15:54:38 +0530

    Added solution - LeetHub

---
+++ Kth smallest element - GFG/kth-smallest-element.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    
+		    int arr[]=new int[n];
+		    
+		    for(int i=0;i<n;i++)
+		    arr[i]=sc.nextInt();
+		    
+		    int k=sc.nextInt();
+		    Solution ob = new Solution();
+		    out.println(ob.kthSmallest(arr, 0, n-1, k));
+		}
+		out.flush();
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    { 
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        for(int i = 0; i < arr.length; i++){
+            pq.add(arr[i]);
+        }
+        for(int i = 0; i < k - 1; i++){
+            pq.poll();
+        }
+        return pq.poll();
+    } 
+}
