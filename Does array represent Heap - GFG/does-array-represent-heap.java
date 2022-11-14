f8b0aad4fc8c0a3f8c339c75e97c4ba59ed58793
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-11-14 13:27:32 +0530

    Added solution - LeetHub

---
+++ Does array represent Heap - GFG/does-array-represent-heap.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.countSub(a, n) ? 1:0);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    
+    public boolean countSub(long arr[], long n)
+    {
+        for(int i = 0; i < arr.length; i++){
+            if((2 * i + 1) < arr.length){
+                if(arr[2 * i + 1] > arr[i]){
+                    return false;
+                }
+            }
+            if((2 * i + 2) < arr.length){
+                if(arr[2 * i + 2] > arr[i]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
