5f9d0d3ff203074b8e64f0b4f84343e9feecae57
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-11-15 23:12:52 +0530

    Added solution - LeetHub

--- Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.java
+++ Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.java
@@ -31,19 +31,39 @@ class GFG{
 
 //User function Template for Java
 
-
+class Pair implements Comparable<Pair>{
+    int value;
+    int row;
+    int col;
+    Pair(int value, int row, int col){
+        this.value = value;
+        this.row = row;
+        this.col = col;
+    }
+    public int compareTo(Pair o){
+        return this.value - o.value;
+    }
+}
 class Solution
 {
     //Function to merge k sorted arrays.
-    public static ArrayList<Integer> mergeKArrays(int[][] arr,int K) 
+    public static ArrayList<Integer> mergeKArrays(int[][] arr,int k) 
     {
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
         ArrayList<Integer> list = new ArrayList<>();
-        for(int[] temp : arr){
-            for(int num : temp){
-                list.add(num);
+        for(int i = 0; i < arr.length; i++){
+            Pair pair = new Pair(arr[i][0], i, 0);
+            pq.add(pair);
+        }
+        while(!pq.isEmpty()){
+            Pair curr = pq.poll();
+            list.add(curr.value);
+            int row = curr.row;
+            if(curr.col + 1 < arr[row].length){
+                Pair pair = new Pair(arr[row][curr.col + 1], row, curr.col + 1);
+                pq.add(pair);
             }
         }
-        Collections.sort(list);
         return list;
     }
 }
