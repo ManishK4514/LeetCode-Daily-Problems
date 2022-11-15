778e84c9a4b67101c26651b474a10f0a9c85033d
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-11-15 10:49:25 +0530

    Added solution - LeetHub

---
+++ Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+class GFG{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t > 0){
+			int n = sc.nextInt();
+			int[][] a = new int[n][n];
+			for(int i = 0; i < n; i++)
+				for(int j = 0; j < n; j++)
+					a[i][j] = sc.nextInt();
+			Solution T = new Solution();
+			ArrayList<Integer> arr= T.mergeKArrays(a, n);
+			for(int i = 0; i < n*n ; i++)
+			    System.out.print(arr.get(i)+" ");
+		    System.out.println();
+		    
+		    t--;
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    //Function to merge k sorted arrays.
+    public static ArrayList<Integer> mergeKArrays(int[][] arr,int K) 
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int[] temp : arr){
+            for(int num : temp){
+                list.add(num);
+            }
+        }
+        Collections.sort(list);
+        return list;
+    }
+}
