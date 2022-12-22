12f1dd95eacaa84bb63fbc27c4cdfb6c58af68e1
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-12-22 15:21:47 +0530

    Time: 89 ms (54.33%), Space: 65.9 MB (81.73%) - LeetHub

---
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.java
@@ -0,0 +1,38 @@
+class Solution {
+    private int[] res;
+    private int[] count;
+    private List<HashSet<Integer>> tree;
+    public int[] sumOfDistancesInTree(int n, int[][] edges) {
+        tree = new ArrayList<HashSet<Integer>>();
+        res = new int[n];
+        count = new int[n];
+        for(int i = 0; i < n; i++){
+            tree.add(new HashSet<Integer>());
+        }
+        for(int[] e : edges){
+            tree.get(e[0]).add(e[1]);
+            tree.get(e[1]).add(e[0]);
+        }
+        postOrder(0, -1);
+        preOrder(0, -1);
+        return res;
+    }
+
+    public void postOrder(int root, int pre){
+        for(int i : tree.get(root)) {
+            if(i == pre) continue;
+            postOrder(i, root);
+            count[root] += count[i];
+            res[root] += res[i] + count[i];
+        }
+        count[root]++;
+    }
+
+    public void preOrder(int root, int pre){
+        for(int i : tree.get(root)){
+            if(i == pre) continue;
+            res[i] = res[root] - count[i] + count.length - count[i];
+            preOrder(i, root);
+        }
+    }
+}
