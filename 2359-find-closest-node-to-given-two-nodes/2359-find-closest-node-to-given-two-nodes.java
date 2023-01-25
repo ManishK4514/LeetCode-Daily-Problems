cbf9beb6d35c2938a6cdd5939ecc17a4ce94a91c
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-01-26 00:19:05 +0530

    Time: 165 ms (19.76%), Space: 158.6 MB (5.99%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -1,27 +1,43 @@
 class Solution {
-    public void dfs(int node, int[] edges, int[] dist, boolean[] visited) {
-        visited[node] = true;
-        int adjacent = edges[node];
-        if (adjacent != -1 && !visited[adjacent]) {
-            dist[adjacent] = dist[node] + 1;
-            dfs(adjacent, edges, dist, visited);
+    public void dfs(int[] dist, int node, int[] vis, ArrayList<ArrayList<Integer>> adj, int d){
+        vis[node] = 1;
+        dist[node] += d;
+        for(int it : adj.get(node)){
+            if(vis[it] == 0){
+                dfs(dist, it, vis, adj, d + 1);
+            }
         }
     }
     public int closestMeetingNode(int[] edges, int node1, int node2) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         int n = edges.length;
-        int ans = -1;
-        int minDist = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<Integer>());
+        }
+        for(int i = 0; i < n; i++){
+            if(edges[i] != -1)adj.get(i).add(edges[i]);
+        }
+        int[] vis1 = new int[n];
+        int[] vis2 = new int[n];
+
         int[] dist1 = new int[n];
         int[] dist2 = new int[n];
-        boolean[] vis1 = new boolean[n];
-        boolean[] vis2 = new boolean[n];
-        dfs(node1, edges, dist1, vis1);
-        dfs(node2, edges, dist2, vis2);
+        
+        Arrays.fill(dist1, -1);
+        Arrays.fill(dist2, -1);
+        
+        dfs(dist1, node1, vis1, adj, 1);
+        dfs(dist2, node2, vis2, adj, 1);
+        
+        int minDis = Integer.MAX_VALUE;
+        int ans = -1;
 
-        for (int currNode = 0; currNode < n; currNode++) {
-            if (vis1[currNode] && vis2[currNode] && minDist > Math.max(dist1[currNode], dist2[currNode])) {
-                minDist = Math.max(dist1[currNode], dist2[currNode]);
-                ans = currNode;
+        for(int i = 0; i < n; i++){
+            if(dist1[i] != -1 && dist2[i] != -1) {
+                if(minDis > Math.max(dist1[i], dist2[i])){
+                    minDis = Math.max(dist1[i], dist2[i]);
+                    ans = i;
+                }
             }
         }
         return ans;
