35fe77218d55e00771b98430dabf17f6ec4f153d
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-01-29 21:35:11 +0530

    Time: 67 ms (87.11%), Space: 133.7 MB (46.43%) - LeetHub

---
+++ 0460-lfu-cache/0460-lfu-cache.java
@@ -0,0 +1,102 @@
+public class LFUCache {
+    class Node {
+        int key, value, count;
+        Node prev, next;
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+            count = 1;
+        }
+    }
+    
+    class DLList {
+        Node head, tail;
+        int size;
+        DLList() {
+            head = new Node(0, 0);
+            tail = new Node(0, 0);
+            head.next = tail;
+            tail.prev = head;
+        }
+        
+        void add(Node node) {
+            head.next.prev = node;
+            node.next = head.next;
+            node.prev = head;
+            head.next = node;
+            size++;
+        }
+        
+        void remove(Node node) {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            size--;
+        }
+        
+        Node removeLast() {
+            if (size > 0) {
+                Node node = tail.prev;
+                remove(node);
+                return node;
+            }
+            else return null;
+        }
+    }
+    
+    int capacity, size, min;
+    Map<Integer, Node> nodeMap;
+    Map<Integer, DLList> countMap;
+    public LFUCache(int capacity) {
+        this.capacity = capacity;
+        nodeMap = new HashMap<>();
+        countMap = new HashMap<>();
+    }
+    
+    public int get(int key) {
+        Node node = nodeMap.get(key);
+        if (node == null) return -1;
+        update(node);
+        return node.value;
+    }
+    
+    public void put(int key, int value) {
+        if (capacity == 0) return;
+        Node node;
+        if (nodeMap.containsKey(key)) {
+            node = nodeMap.get(key);
+            node.value = value;
+            update(node);
+        }
+        else {
+            node = new Node(key, value);
+            nodeMap.put(key, node);
+            if (size == capacity) {
+                DLList lastList = countMap.get(min);
+                nodeMap.remove(lastList.removeLast().key);
+                size--;
+            }
+            size++;
+            min = 1;
+            DLList newList = countMap.getOrDefault(node.count, new DLList());
+            newList.add(node);
+            countMap.put(node.count, newList);
+        }
+    }
+    
+    private void update(Node node) {
+        DLList oldList = countMap.get(node.count);
+        oldList.remove(node);
+        if (node.count == min && oldList.size == 0) min++; 
+        node.count++;
+        DLList newList = countMap.getOrDefault(node.count, new DLList());
+        newList.add(node);
+        countMap.put(node.count, newList);
+    }
+}
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
