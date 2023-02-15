b4624e7a2033e258cb051256443dc75da7eecc64
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-02-15 17:47:02 +0530

    Time: 835 ms (5.04%), Space: 28.5 MB (46.31%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -1,15 +1,15 @@
 class Solution {
-    public List<Integer> addToArrayForm(int[] num, int k) {
-        List<Integer> res = new ArrayList<>();
-        for(int i = num.length - 1; i >= 0; i--){
-            res.add((num[i] + k) % 10);
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        vector<int> res;
+        for(int i = num.size() - 1; i >= 0; i--){
+            res.insert(res.begin(), (num[i] + k) % 10);
             k = (num[i] + k) / 10;
         }
         while (k > 0) {
-            res.add(k % 10);
+            res.insert(res.begin(), k % 10);
             k /= 10;
         }
-        Collections.reverse(res);
         return res;
     }
-}
+};
