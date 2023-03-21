0919cd384652f15b427fb71adb8b8f9d51a974b7
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-03-21 15:58:20 +0530

    Time: 6 ms (34.17%), Space: 60.1 MB (27.27%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -1,8 +1,7 @@
 class Solution {
-public:
-    long long zeroFilledSubarray(vector<int>& nums) {
-        long long ans = 0, zeros = 0;
-        for(int i = 0; i < nums.size(); i++){
+    public long zeroFilledSubarray(int[] nums) {
+        long ans = 0, zeros = 0;
+        for(int i = 0; i < nums.length; i++){
             if(nums[i] != 0) {
                 ans += zeros * (zeros + 1) / 2;
                 zeros = 0;
@@ -12,4 +11,4 @@ public:
         ans += zeros * (zeros + 1) / 2;
         return ans;
     }
-};
+}
