b9cc52d5e994cb3b0f75f389651db0c81c5e4887
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-05-08 14:14:30 +0530

    Time: 15 ms (56.74%), Space: 11.1 MB (92.07%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-        int n = mat.size(), sum = 0;
+        int n = mat.size(), sum = 0, j = mat.size() - 1;
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                // if-else automatically ignore the middle element of odd size matrix because that is the only position could be same and we have to take only once
-                if(i == j) sum += mat[i][j];
-                else if(i + j == n - 1) sum += mat[i][j];
-            }
+            sum += mat[i][i];
+            sum += mat[i][j--];
         }
+        if(n % 2 != 0) sum -= mat[n/2][n/2];
         return sum;
     }
 };
