3cd28550d974a1eb4730c3d06096211ea32285cd
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-06-30 22:13:37 +0530

    Added solution - LeetHub

---
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+   
+    // Function for finding maximum and value pair
+    public static int lenOfLongSubarr (int arr[], int n, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0, ans = 0;
+        map.put(0, -1);
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+            
+            if(map.containsKey(sum - target)){
+                ans = Math.max(ans, i - map.get(sum - target));
+            }
+            if(!map.containsKey(sum)){
+                map.put(sum, i);
+            }
+        }
+        return ans;
+    }
+    
+    
+}
+
+
