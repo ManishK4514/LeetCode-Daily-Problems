c0128bfef89f8fab88cd649436dd15bda4484282
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-07-04 19:35:24 +0530

    Added solution - LeetHub

--- Partitions with Given Difference - GFG/partitions-with-given-difference.java
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.java
@@ -32,34 +32,33 @@ class GFG{
 //Back-end complete function Template for Java
 
 class Solution{
-    int mod = (int) (1e9 + 7);
-    public int helper(int i, int targetSum, int[] arr, int[][] dp){
-        // base case
-        if(i == 0){
-            if(targetSum == 0 && arr[0] == 0) return 2;
-            if(targetSum == arr[i] || targetSum == 0) return 1;
-            return 0;
-        }
-        
-        if(dp[i][targetSum] != -1) return dp[i][targetSum];
-        
-        // take
-        int take = 0;
-        if(targetSum >= arr[i]) take = helper(i - 1, targetSum - arr[i], arr, dp);
-        
-        // not take
-        int notTake = helper(i - 1, targetSum, arr, dp);
-        
-        return dp[i][targetSum] =  (take + notTake) % mod;
-    }
     public int countPartitions(int n, int d, int arr[]){
-        int totalSum = 0;
+        int totalSum = 0, mod = (int) (1e9 + 7);
         for(int i = 0; i < n; i++) totalSum += arr[i];
         
         if(totalSum - d < 0 || ((totalSum - d) % 2) != 0) return 0;
         
-        int[][] dp = new int[n][totalSum + 1];
-        for(int[] it : dp) Arrays.fill(it, -1);
-        return helper(n - 1, (totalSum - d)/2, arr, dp);
+        int targetSum = (totalSum - d)/2;
+        
+        int[][] dp = new int[n][targetSum + 1];
+        
+        if(arr[0] == 0) dp[0][0] = 2;
+        else dp[0][0] = 1;
+        
+        if(arr[0] != 0 && arr[0] <= targetSum) dp[0][arr[0]] = 1;
+        
+        for(int i = 1; i < n; i++){
+            for(int sum = 0; sum <= targetSum; sum++){
+                // take
+                int take = 0;
+                if(sum >= arr[i]) take = dp[i - 1][sum - arr[i]];
+                
+                // not take
+                int notTake = dp[i - 1][sum];
+                
+                dp[i][sum] =  (take + notTake) % mod;
+            }
+        }
+        return dp[n - 1][targetSum];
     }
 }
