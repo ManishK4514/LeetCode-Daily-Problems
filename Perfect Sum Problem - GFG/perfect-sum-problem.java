778282c88230fbe71b9b564400b1d8d654e80cd4
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-07-04 15:18:25 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.java
+++ Perfect Sum Problem - GFG/perfect-sum-problem.java
@@ -26,27 +26,41 @@ class GfG
 
 
 class Solution{
-    int mod = (int)(1e9 + 7);
-    public int helper(int i, int[] arr, int target, int[][] dp){
-        if(i < 0) {
-            if(target == 0) return 1;
-            return 0;
+    public void reverseArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+    
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+    
+            start++;
+            end--;
         }
-        
-        if(dp[i][target] != -1) return dp[i][target];
-        
-        // take 
-        int take = 0;
-        if(target >= arr[i]) take = helper(i - 1, arr, target - arr[i], dp);
-        
-        // notTake
-        int notTake = helper(i - 1, arr, target, dp);
-        
-        return dp[i][target] = ((take % mod) + (notTake % mod)) % mod;
     }
-	public int perfectSum(int[] arr,int n, int sum) {
-	    int[][] dp = new int[n][sum + 1];
-	    for(int[] it : dp) Arrays.fill(it, -1);
-	    return helper(n - 1, arr, sum, dp) % mod;
+	public int perfectSum(int[] arr,int n, int target) {
+	    if(arr[0] == 0) reverseArray(arr);
+	    
+	    int mod = (int)(1e9 + 7);
+	    int[][] dp = new int[n][target + 1];
+	    for(int i = 0; i < n; i++) dp[i][0] = 1;
+	    
+	    if(arr[0] <= target) dp[0][arr[0]] = 1;
+	    
+	    for(int i = 1; i < n; i++){
+	        for(int sum = 0; sum <= target; sum++){
+	            // take
+	            int take = 0;
+	            if(sum >= arr[i]) take = dp[i - 1][sum - arr[i]];
+        
+                // notTake
+                int notTake = dp[i - 1][sum];
+                
+                dp[i][sum] = ((take % mod) + (notTake % mod)) % mod;
+	        }
+	    }
+	    
+	    return dp[n - 1][target] % mod;
 	} 
 }
