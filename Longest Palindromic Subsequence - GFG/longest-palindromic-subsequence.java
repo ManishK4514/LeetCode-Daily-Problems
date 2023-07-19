3bc7b73d74e6bb560193f8b1d38de3e4462ca936
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-07-19 18:36:44 +0530

    Added solution - LeetHub

--- Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.java
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.java
@@ -25,27 +25,24 @@ class GfG
 //User function Template for Java
 
 class Solution
-{   
-    public int helper(String s, int i, int j, int[][] dp){
+{
+    public int helper(int i, int j, String s, int[][] dp){
         if(i == j) return 1;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        // if length is 2 and both charcter are equals
-        if((j - i + 1) == 2 && s.charAt(i) == s.charAt(j)) return 2;
         
-        // if first and last characters are equal then call for the remaining string
-        if(s.charAt(i) == s.charAt(j)) {
-            return dp[i][j] = 2 + helper(s, i + 1, j - 1, dp);
+        if(j - i == 1 && s.charAt(i) == s.charAt(j)) return 2;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(s.charAt(i) == s.charAt(j)){
+            return dp[i][j] = 2 + helper(i + 1, j - 1, s, dp);
         }
-
-        return dp[i][j] = Math.max(helper(s, i + 1, j, dp), helper(s, i, j - 1, dp));
+        else return dp[i][j] = Math.max(helper(i + 1, j, s, dp), helper(i, j - 1, s, dp));
     }
-    public int longestPalinSubseq(String s)
+    public int longestPalinSubseq(String S)
     {
-        int n = s.length();
+        int n = S.length();
         int[][] dp = new int[n][n];
         for(int[] it : dp) Arrays.fill(it, -1);
-        return helper(s, 0, n - 1, dp);
+        return helper(0, S.length() - 1, S, dp);
     }
 }
