c9b2f6810ca58921e6d0e3677302ab2f8f6e4c91
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-08-14 18:24:30 +0530

    Added solution - LeetHub

---
+++ Non Repeating Numbers - GFG/non-repeating-numbers.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            int[] v = new int[2 * n + 2];
+            for(int i = 0; i < 2 * n + 2; i++)
+            {
+                v[i] = Integer.parseInt(S[i]);
+            }
+            Solution ob = new Solution();
+            int[] ans = ob.singleNumber(v);
+            for(int i = 0; i < ans.length; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] singleNumber(int[] nums)
+    {
+        int n = nums.length, idx = 0;
+        Arrays.sort(nums);
+        int[] ans = new int[2];
+        for(int i = 0; i < n; i+=2) {
+            if((i == n - 1 && idx == 1) || nums[i] != nums[i + 1]) {
+                ans[idx++] = nums[i];                
+                if(idx == 2) break;
+                i--;
+            }
+        }
+        return ans;
+    }
+}
