ffd68ee5cd7f44d043c091e4c869228da049c86b
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-08-25 14:22:32 +0530

    Added solution - LeetHub

---
+++ Palindrome String - GFG/palindrome-string.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            
+            Solution ob = new Solution();
+            System.out.println(ob.isPalindrome(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int isPalindrome(String S) {
+        int n = S.length();
+        for(int i = 0; i < n/2; i++) {
+            if(S.charAt(i) != S.charAt(n - i - 1)) return 0;
+        }
+        return 1;
+    }
+};
