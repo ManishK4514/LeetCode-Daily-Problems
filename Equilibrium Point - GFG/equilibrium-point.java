422b891b053e083ee8dfa820e1de0a3d2c7cf11a
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-09-23 18:11:00 +0530

    Added solution - LeetHub

---
+++ Equilibrium Point - GFG/equilibrium-point.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            
+            //taking input n
+            int n = Integer.parseInt(br.readLine().trim());
+            long arr[] = new long[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //adding elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = Long.parseLong(inputLine[i]);
+            }
+
+            Solution ob = new Solution();
+            
+            //calling equilibriumPoint() function
+            System.out.println(ob.equilibriumPoint(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    
+    // a: input array
+    // n: size of array
+    // Function to find equilibrium point in the array.
+    public static int equilibriumPoint(long arr[], int n) {
+
+        long tsum = 0;
+        for(long it : arr) tsum += it;
+        
+        long csum = 0;
+        int i = 0;
+        for(long it : arr) {
+            csum += it;
+            if(tsum  - csum == csum - it) return i + 1;
+            i++;
+        }
+        return -1;
+    }
+}
