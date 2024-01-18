e79ee5519e2e393fd836f06db1f31d9c38d542d1
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2024-01-18 18:24:00 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (30.99%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,13 @@
 class Solution {
-    public int count(int n, int steps, int[] dp){
-        if(steps > n) return 0;
-        if(steps == n) return 1;
-        if(dp[steps] != -1){
-            return dp[steps];
-        }
-        dp[steps] = count(n, steps + 1, dp) + count(n, steps + 2, dp);
-        return dp[steps];
-    }
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return count(n, 0, dp);
+        if(n < 2) return 1;
+        int curr = 0, prev1 = 1, prev2 = 1;
+        for(int i = 2; i <= n; i++) {
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return curr;
     }
 }
