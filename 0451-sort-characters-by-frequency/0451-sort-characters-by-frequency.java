5835fbbf08e7886927012936cfda1b3f40a93387
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2024-02-07 20:26:05 +0530

    Time: 13 ms (77.46%), Space: 45 MB (76.68%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,18 +1,36 @@
+class Pair {
+    char ch;
+    int freq;
+    Pair(char ch, int freq) {
+        this.ch = ch;
+        this.freq = freq;
+    }
+}
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();        
-        for(int i = 0; i < s.length(); i++){
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-        }   
-        PriorityQueue<Character> pq = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
-        pq.addAll(map.keySet());
+        }
+
+        int n = map.size();
+        Pair[] arr = new Pair[n];
+        int idx = 0;
+        for(char key : map.keySet()) {
+            arr[idx++] = new Pair(key, map.get(key));
+        }
+
+        Arrays.sort(arr, (a, b)->{
+            return b.freq - a.freq;
+        });
+
         StringBuilder sb = new StringBuilder();
-        while(!pq.isEmpty()){
-            char curr = pq.remove();
-            for(int i = 0; i < map.get(curr); i++){
-                sb.append(curr);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < arr[i].freq; j++) {
+                sb.append(arr[i].ch);
             }
         }
+
         return sb.toString();
     }
 }
