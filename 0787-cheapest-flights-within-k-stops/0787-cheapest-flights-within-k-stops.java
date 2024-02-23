971c3e6aba194c4ecb4611bd979cc798582aa662
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2024-02-23 22:19:20 +0530

    Time: 6 ms (86.77%), Space: 43.9 MB (96.78%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,50 +1,48 @@
-class Pair{
-    int first;
-    int second;
-    Pair(int first, int second){
-        this.first = first;
-        this.second = second;
+class Tuple {
+    int node;
+    int dist;
+    int stops;
+
+    Tuple(int node, int dist, int stops) {
+        this.node = node;
+        this.dist = dist;
+        this.stops = stops;
     }
 }
-class Touple{
-    int first;
-    int second;
-    int third;
-    Touple(int first, int second, int third){
-        this.first = first;
-        this.second = second;
-        this.third = third;
-    }
-}
+
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            adj.add(new ArrayList<Pair>());
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        for(int[] flight : flights) {
+            adj.get(flight[0]).add(new int[]{flight[1], flight[2]});
         }
-        for(int i = 0; i < flights.length; i++){
-            adj.get(flights[i][0]).add(new Pair(flights[i][1], flights[i][2]));
-        }
-        int[] dist = new int[n];
-        for(int i = 0; i < n; i++) dist[i] = Integer.MAX_VALUE;
-        Queue<Touple> q = new LinkedList<>();
-        q.add(new Touple(0, src, 0));
-        while(!q.isEmpty()){
-            Touple it = q.remove();
-            int stops = it.first;
-            int node = it.second;
-            int cost = it.third;
 
-            if(stops > k) continue;
-            for(Pair iter : adj.get(node)){
-                int adjNode = iter.first;
-                int edgeWeight = iter.second;
-                if(cost + edgeWeight < dist[adjNode]){
-                    dist[adjNode] = cost + edgeWeight;
-                    q.add(new Touple(stops + 1, adjNode, dist[adjNode]));
+        int[] distance = new int[n];
+        Arrays.fill(distance, (int)(1e9));
+
+        Queue<Tuple> pq = new LinkedList<>();
+
+        pq.add(new Tuple(src, 0, 0));
+        distance[src] = 0;
+
+        while(!pq.isEmpty()) {
+            Tuple t = pq.remove();
+            int node = t.node;
+            int dist = t.dist;
+            int stops = t.stops;
+
+            for(int[] it : adj.get(node)) {
+                int currNode = it[0];
+                int currDist = dist + it[1];
+
+                if(distance[currNode] > currDist && stops <= k) {
+                    distance[currNode] = currDist;
+                    pq.add(new Tuple(currNode, currDist, stops + 1));
                 }
             }
         }
-        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+
+        return distance[dst] == (int)(1e9) ? -1 : distance[dst];
     }
 }
