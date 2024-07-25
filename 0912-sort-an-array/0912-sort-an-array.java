3856b331d7da7517053d700068d47062d964fcdc
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2024-07-25 23:51:23 +0530

    Time: 17 ms (89.75%), Space: 55.2 MB (90.22%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,44 +1,6 @@
 class Solution {
-    public void merge(int[] arr, int start, int end){
-        // Calculating mid element
-        int mid = start + (end - start)/2;
-
-        // creating a temp Array
-        int[] temp = new int[end - start + 1];
-        int i = start;
-        int j = mid + 1;
-        int k = 0;
-        while(i <= mid && j <= end){
-            if(arr[i] <= arr[j]){
-                temp[k++] = arr[i++];
-            }
-            else{
-                temp[k++] = arr[j++];
-            }
-        }
-        while(i <= mid){
-            temp[k++] = arr[i++];
-        }
-        while(j <= end){
-            temp[k++] = arr[j++];
-        }
-
-        // Copying all Elements of temp into Original Array.
-        for(int m = 0, n = start; m < temp.length; m++, n++){
-            arr[n] = temp[m];
-        }
-    }
-    public void mergeSort(int[] arr, int start, int end){
-        if(start >= end){
-            return;
-        }
-        int mid = start + (end - start)/2;
-        mergeSort(arr, start, mid);
-        mergeSort(arr, mid + 1, end);
-        merge(arr, start, end);
-    }
     public int[] sortArray(int[] nums) {
-        mergeSort(nums, 0, nums.length - 1);
+        Arrays.sort(nums);
         return nums;
-    }   
+    }
 }
