b03d7073ea3e4fec4bb94c01f21f4c67889b5f09
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2024-08-21 00:12:30 +0530

    Time: 9 ms (55.58%), Space: 44.1 MB (37.62%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.java
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -1,24 +1,26 @@
 class Solution {
-    public int helper(int i, int[] piles, int m, int[][] dp){
+    public int helper(int i, int[] piles, int m, int[][] dp) {
         if(i >= piles.length) return 0;
+
         if(dp[i][m] != -1) return dp[i][m];
-        int ans = Integer.MIN_VALUE, total = 0;
-        for(int j = 0; j < 2 * m; j++){
-            if(i + j < piles.length) total += piles[i + j];
-            ans = Math.max(ans, total - helper(i + j + 1, piles, Math.max(m, j + 1), dp));
+        
+        int alice = 0, aliceMinusBob = Integer.MIN_VALUE;
+        for(int x = 0; x < 2 * m; x++) {
+            if(i + x < piles.length) alice += piles[i + x];
+
+            aliceMinusBob = Math.max(aliceMinusBob, alice - helper(i + x + 1, piles, Math.max(m, x + 1), dp));
         }
-        return dp[i][m] = ans;
+
+        return dp[i][m] = aliceMinusBob;
     }
     public int stoneGameII(int[] piles) {
-        int[][] dp = new int[piles.length][201];
+        int alicePlusBob = 0, n = piles.length;
+        for(int pile : piles) alicePlusBob += pile;
+
+        int[][] dp = new int[n][201];
         for(int[] it : dp) Arrays.fill(it, -1);
-        int sum = 0;
-        for(int it : piles) sum += it;
-        // we are getting a - b
-        int diff = helper(0, piles, 1, dp);
-        // sum = a + b & diff = a - b
-        // (sum + diff) -> (a + b + a - b) = 2a
-        // and the answer is 2a/2 = a 
-        return (sum + diff)/2;
+        int aliceMinusBob = helper(0, piles, 1, dp);
+
+        return (alicePlusBob + aliceMinusBob)/2;
     }
 }
