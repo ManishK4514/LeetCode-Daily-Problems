a6a7c1852d82c420c6bf26193d025241adb6933f
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2024-10-24 23:15:04 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (32.68%) - LeetHub

---
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if (root1 == null || root2 == null)
+            return root1 == root2;
+
+        return root1.val == root2.val && ((flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right))
+                || (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left)));
+    }
+}
